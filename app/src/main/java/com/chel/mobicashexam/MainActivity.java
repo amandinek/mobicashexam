caa29f28fbb0fdca508da949f801f18c6ed0e830
Sangwa-C <chelseasabinesangwa@gmail.com>
2020-01-24 18:31:02 +0200

    add a onClickListener

--- app/src/main/java/com/chel/mobicashexam/MainActivity.java
+++ app/src/main/java/com/chel/mobicashexam/MainActivity.java
@@ -2,13 +2,29 @@ package com.chel.mobicashexam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        btn =(Button) findViewById( R.id.btn );
+        btn.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v== btn){
+            Intent intent = new Intent( MainActivity.this,Register.class );
+                    intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK );
+                    startActivity( intent );
+                    finish();
+        }
     }
 }
