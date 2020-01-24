5c32a08655659e9b48ffe7f4ae26de94a776799b
Sangwa-C <chelseasabinesangwa@gmail.com>
2020-01-24 15:49:59 +0200

    authernthication

--- app/src/main/java/com/chel/mobicashexam/Register.java
+++ app/src/main/java/com/chel/mobicashexam/Register.java
@@ -3,13 +3,21 @@ package com.chel.mobicashexam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
     private Button signUp;
     private Button cancel;
     private EditText firstName,lastName,email,password,phoneNumber;
+
 
 
 
@@ -19,14 +27,38 @@ public class Register extends AppCompatActivity {
 //        Realm.init(context);
         setContentView( R.layout.activity_register );
 
-        signUp=(Button)findViewById( R.id.signUp );
-        cancel=(Button)findViewById( R.id.cancel );
-        firstName=(EditText)findViewById( R.id.firstName );
-        lastName=(EditText)findViewById( R.id.lastName );
-        email=(EditText)findViewById( R.id.email );
-        password=(EditText)findViewById( R.id.password );
-        phoneNumber=(EditText)findViewById( R.id.PhoneNbr);
+        signUp = (Button) findViewById( R.id.signUp );
+        cancel = (Button) findViewById( R.id.cancel );
+        firstName = (EditText) findViewById( R.id.firstName );
+        lastName = (EditText) findViewById( R.id.lastName );
+        email = (EditText) findViewById( R.id.email );
+        password = (EditText) findViewById( R.id.password );
+        phoneNumber = (EditText) findViewById( R.id.PhoneNbr );
 
+        signUp.setOnClickListener(  this );
+        cancel.setOnClickListener( this );
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+
+        createAuthListner();
+//        createAuthProgressDialog();}
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v== signUp){
+            createnUser();
+        }
+
+        if(v== cancel){
+//            something
+        }
 
     }
+    public void createnUser(){
+
+    }
+
+
 }
