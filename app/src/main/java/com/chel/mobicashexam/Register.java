a3f6d68965c3dd93e2a7c5c72173945f6b577ce4
amandinek <karambiziga@gmail.com>
2020-01-27 13:45:12 +0200

    add button

--- app/src/main/java/com/chel/mobicashexam/Register.java
+++ app/src/main/java/com/chel/mobicashexam/Register.java
@@ -6,29 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.chel.mobicashexam.model.User;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
     FirebaseAuth auth;
-    FirebaseAuth.AuthStateListener authStateListener;
+//    FirebaseAuth.AuthStateListener authStateListener;
     FirebaseDatabase db;
     DatabaseReference users;
 
@@ -43,7 +38,6 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-//        Realm.init(context);
         setContentView( R.layout.activity_register );
 
         signUp = (Button) findViewById( R.id.signUp );
@@ -149,22 +143,6 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
 
 
     }
-//    public void createAuthListner(){
-//        authStateListener = new FirebaseAuth.AuthStateListener() {
-//            @Override
-//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-//                final FirebaseUser user= firebaseAuth.getCurrentUser();
-//                if(user !=null){
-//                    Intent intent = new Intent( Register.this,UserDetails.class );
-//                    intent.setFlags( Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK );
-//                    startActivity( intent );
-//                    finish();
-//                }
-//            }
-//        };
-//    }
-
-
 
 
 }
